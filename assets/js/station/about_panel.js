719b4d33e75566d471c710b84643d8878947e3d8
“muweikun” <2694455419@qq.com>
2024-11-04 21:47:54 +0800

    删除所有的东东

--- assets/js/station/about_panel.js
+++
@@ -1,5 +0,0 @@
-let AboutPanel = function(pannel_id) {
-    let that = Panel(pannel_id, '关于');
-
-    return that;
-};
--- assets/js/station/device_panel.js
+++
@@ -1,5 +0,0 @@
-let DevicePanel = function(pannel_id) {
-    let that = Panel(pannel_id, '设备管理');
-
-    return that;
-};
